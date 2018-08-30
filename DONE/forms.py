4253fe253040cd70034c49932493991183bac3af
jlochran <jlochran@gmail.com>
2018-08-30 13:27:51 -0400

    added accounts app and styling

--- DONE/forms.py
+++ DONE/forms.py
@@ -2,6 +2,29 @@ from django import forms
 from .models import Project
 
 class ProjectForm(forms.ModelForm):
+                    
+    title = forms.CharField()
+
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Provide a description of your project...',
+            'rows' :'3',
+        }
+    ))
+    location = forms.CharField(widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Provide a location for your project...',
+            'rows' :'3',
+        }
+    ))
+
+
+
+
+
+
     class Meta:
         model = Project
         fields = ['title', 'description', 'type', 'location', 'support', 'phone', 'contact_me', 'timeframe']
--- LT/settings.py
+++ LT/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DONE',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'LT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['LT//templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'LT/static/')
+
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
