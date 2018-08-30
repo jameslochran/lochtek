7ce6a9300a88045e867e4e9bb6d694b1b6967af8
jlochran <jlochran@gmail.com>
2018-08-30 16:04:57 -0400

    fixing static files

--- LT/settings.py
+++ LT/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['LT//templates'],
+        'DIRS': ['LT/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
